60ae61d9502e2320ce220b2066868ee90f1fdc48
Jordan Ross <jorross@umich.edu>
2022-01-27 11:50:40 -0800

    added if logic to prevent list from trying to pull from empty localStorage

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,10 +1,11 @@
 var highscores_list_el = $('#score-list');
 var clear_btn_div = $('#start-btn-div');
-
-for (var i=0; i<localStorage.length; i++) {
-    highscores_list_el.append(
-        '<li class="w-100">' + localStorage.key(i) + ': ' + localStorage.getItem(localStorage.key(i)) + '</li>'
-    )
+if (localStorage.length > 0) {
+    for (var i=0; i<localStorage.length; i++) {
+        highscores_list_el.append(
+            '<li class="w-100">' + localStorage.key(i) + ': ' + localStorage.getItem(localStorage.key(i)) + '</li>'
+        )
+    }
 }
 
 clear_btn_div.on('click', '.btn-danger', function() {
